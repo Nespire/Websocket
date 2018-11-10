79d1d3efdc000069d58379a6ad581c100580883b
Nespire <33282623+Nespire@users.noreply.github.com>
2018-11-10 17:02:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let ws = new WebSocket("ws://wsei.edu.pl:8010");
 
+
+
+
+
 ws.onopen = function() {
     ws.send("Message to send");
     console.log("Jak to odpalic... ?")
 }
-  
+
+document.querySelector("#wsSend").addEventListener('click', function(){
+    let sendMsg = {
+        title: document.querySelector('#name').value,
+        body: document.querySelector('#wsMsg').value,
+    }
+    wsSend(JSON.stringify(sendMsg))
+})
+
+
 ws.onmessage = function (evt) { 
     var received_msg = evt.data;
     console.log("Message is received...")
@@ -12,4 +25,4 @@ ws.onmessage = function (evt) {
   
 ws.onclose = function() { 
     console.log("Connection is closed...")
-};
+}
