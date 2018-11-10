9bc2620730cb4f090b0abfa7656d7592a2ffd59d
Nespire <33282623+Nespire@users.noreply.github.com>
2018-11-10 16:46:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let ws = new WebSocket("ws://wsei.edu.pl:8010");
+
+ws.onopen = function() {
+    ws.send("Message to send");
+    console.log("Message is sent...")
+}
+  
+ws.onmessage = function (evt) { 
+    var received_msg = evt.data;
+    console.log("Message is received...")
+}
+  
+ws.onclose = function() { 
+    console.log("Connection is closed...")
+};
